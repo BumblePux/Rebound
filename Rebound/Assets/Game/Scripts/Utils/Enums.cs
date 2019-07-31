a28b905b8bd1431e38f3451a803e52239b7ad5b8
BumblePux <53489619+BumblePux@users.noreply.github.com>
2019-07-31 19:16:57 +0100

    Added Utils folder and Enums file
    Enums file will be a central point to contain all project enums.

---
+++ Rebound/Assets/Game/Scripts/Utils/Enums.cs
@@ -0,0 +1,23 @@
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//
+// Name:        Enums.cs
+// Created by:  Pux
+// Created on:  31/07/19
+//
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BumblePux.Rebound.Utils
+{
+    public class Enums
+    {
+        public enum Rotation
+        {
+            Clockwise,
+            AntiClockwise
+        }
+    }
+}
