9fab723fd50a3b2a448666baac1b1648bcb011b8
BumblePux <53489619+BumblePux@users.noreply.github.com>
2019-08-11 15:22:11 +0100

    Created MobileInput component

---
+++ Rebound/Assets/Game/Scripts/Input/MobileInput.cs
@@ -0,0 +1,33 @@
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//
+// Name:        MobileInput.cs
+// Created by:  Pux
+// Created on:  11/08/19
+//
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BumblePux.Rebound.UserInput
+{
+    public class MobileInput : BaseUserInput
+    {
+        public override bool Clicked()
+        {
+            if (Input.touchCount > 0)
+            {
+                if (eventSystem != null)
+                {
+                    if (Input.GetTouch(0).phase == TouchPhase.Began && eventSystem.IsPointerOverGameObject(Input.GetTouch(0).fingerId))
+                        return false;
+                }
+
+                return Input.GetTouch(0).phase == TouchPhase.Began;
+            }
+            else
+                return false;
+        }
+    }
+}
