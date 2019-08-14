a3ed12a46dbbc08624c8b624ed499bfd8a2e054e
BumblePux <53489619+BumblePux@users.noreply.github.com>
2019-08-14 19:53:25 +0100

    Rewrote GameOverController

--- Rebound/Assets/Game/Scripts/UI/GameOverController.cs
+++ Rebound/Assets/Game/Scripts/UI/GameOverController.cs
@@ -2,111 +2,68 @@
 //
 // Name:        GameOver.cs
 // Created by:  Pux
-// Created on:  11/08/19
+// Created on:  13/08/19
 //
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using TMPro;
 using BumblePux.Rebound.Score;
-using BumblePux.Rebound.Utils;
-using BumblePux.Rebound.GameControllers;
 
 namespace BumblePux.Rebound.UI
 {
     public class GameOverController : MonoBehaviour
     {
-        private static GameOverController instance;
-
-        [SerializeField] private GameObject gameOverPanel = default;
-        [SerializeField] private Animator gameOverAnimator = default;
-        [SerializeField] private TMP_Text scoreText = default;
-
-        [Header("External References")]
-        [SerializeField] private SimpleScoreHandler score = default;
-        [SerializeField] private MainMenu mainMenu = default;
-        [SerializeField] private BaseGameController controller = default;
+        public float animationWaitDuration = 1.5f;
+        public Animator gameOverAnimator;
+        public TMP_Text scoreText;
 
         //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         // Public Methods
         //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        public static void SetActive(bool active)
+        public void OnGameOver()
         {
-            instance.gameOverPanel.SetActive(active);
+            SetScore(ScoreManager.Score);
+            gameOverAnimator.SetBool("gameOverAppear", true);
         }
 
         //----------------------------------------
-        public static void ShowGameOverPanel()
+        public void WatchAd()
         {
-            instance.SetScoreText();
-            instance.gameOverAnimator.SetBool("isGameOver", true);
+            Debug.Log("Attempt to show Rewarded Ad");
         }
 
         //----------------------------------------
-        public void OnHomeSelected()
+        public void LoadMainMenu()
         {
-            StopAllCoroutines();
-            StartCoroutine(MainMenu());
+            gameOverAnimator.SetBool("gameOverAppear", false);
+            StartCoroutine(LoadSceneAfterAnimations("MainMenu", animationWaitDuration));
         }
 
         //----------------------------------------
-        public void OnRestartSelected()
+        public void RestartScene()
         {
-            StopAllCoroutines();
-            StartCoroutine(Restart());
-        }
-
-        //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        // Unity Methods
-        //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        private void Awake()
-        {
-            // Initialize Singleton
-            if (instance != null && instance != this)
-            {
-                Destroy(gameObject);
-            }
-            else
-            {
-                instance = this;
-                DontDestroyOnLoad(gameObject);
-            }
+            gameOverAnimator.SetBool("gameOverAppear", false);
+            StartCoroutine(LoadSceneAfterAnimations(SceneManager.GetActiveScene().name, animationWaitDuration));
         }
 
         //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         // Private Methods
         //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        private void SetScoreText()
+        private IEnumerator LoadSceneAfterAnimations(string sceneName, float duration)
         {
-            scoreText.text = score.GetCurrentScore().ToString();
+            yield return new WaitForSeconds(duration);
+
+            SceneManager.LoadScene(sceneName);
         }
 
         //----------------------------------------
-        private IEnumerator MainMenu()
+        private void SetScore(int score)
         {
-            instance.gameOverAnimator.SetBool("isGameOver", false);
-
-            yield return StartCoroutine(WaitForAnimations());
-
-            mainMenu.OnReturnToTitleScreen();
-        }
-
-        //----------------------------------------
-        private IEnumerator Restart()
-        {
-            instance.gameOverAnimator.SetBool("isGameOver", false);
-
-            yield return StartCoroutine(WaitForAnimations());
-
-            controller.GameStart();
-        }
-
-        //----------------------------------------
-        private IEnumerator WaitForAnimations()
-        {
-            yield return new WaitForSeconds(gameOverAnimator.GetCurrentAnimatorStateInfo(0).length);
+            scoreText.text = score.ToString();
         }
     }
 }
